f29e7e7cad9184f01efa00e62f28ae10621ff895
JIFreitas <jfreitasxd@gmail.com>
2021-12-07 16:54:47 +0000

    QRCODE

---
+++ app/src/main/java/estg/djr/artip/Qr.kt
@@ -0,0 +1,11 @@
+package estg.djr.artip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Qr : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr)
+    }
+}
