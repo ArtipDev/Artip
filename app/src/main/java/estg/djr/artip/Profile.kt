7283a1db56b9b0779ac5f9a9a103a891e34508d1
JIFreitas <jfreitasxd@gmail.com>
2022-01-17 16:07:42 +0000

    branch perfil

--- app/src/main/java/estg/djr/artip/Profile.kt
+++ app/src/main/java/estg/djr/artip/Profile.kt
@@ -3,11 +3,25 @@ package estg.djr.artip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import estg.djr.artip.ui.theme.ArtipTheme
 
 class Profile : ComponentActivity() {
@@ -27,7 +41,41 @@ class Profile : ComponentActivity() {
 @Composable
 fun ProfileCompo(visible: Boolean) {
     if(visible) {
-        Text(text = "Hello!")
+        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
+            .padding(10.dp)
+            .border(1.dp, Color(100, 244, 244))){
+            Box(modifier = Modifier.padding(10.dp)) {
+                Image(
+                    painterResource(id = R.drawable.profile_icon),
+                    "Foto Perfil",
+                    modifier = Modifier
+                        .size(150.dp, 150.dp)
+                        .fillMaxSize()
+                        .clip(CircleShape)
+                        .border(2.dp, Color.LightGray, CircleShape)
+                )
+            }
+            Column (horizontalAlignment = Alignment.CenterHorizontally , verticalArrangement = Arrangement.Center,modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp)){
+                Text("João Freitas", fontSize = 30.sp)
+                Text("Porto, Portugal", fontSize = 20.sp)
+            }
+            Row(modifier = Modifier.fillMaxWidth().padding(10.dp), horizontalArrangement = Arrangement.Center) {
+                Row(){
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.size(100.dp, 40.dp)) {
+                        Text("Seguir")
+                    }
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(horizontal = 13.dp).size(100.dp, 40.dp)) {
+                        Text("?")
+                    }
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.size(100.dp, 40.dp)) {
+                        Text("Donation")
+                    }
+                }
+            }
+        }
+
     }
 }
 
